361203ca8422f1aad59ce27ad4303a9fd8fad292
Emina <emina@simplify.ba>
2023-12-22 16:31:00 +0100

    adding json

--- dev.rb
+++ dev.rb
@@ -1,6 +1,7 @@
 require './src/weather_air'
 require 'dotenv/load'
 
-(bosnian, english) = WeatherAir.run
+(bosnian, english, feed) = WeatherAir.run
 File.write('index.html', bosnian)
 File.write('en/index.html', english)
+File.write('feed.json', feed)
